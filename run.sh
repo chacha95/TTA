e788df7408ebac95e34d67610b087ddb301bb447
jinhyuck <ckwlgsur20@gmail.com>
2021-06-09 14:34:53 +0900

    multi gpu

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd gpu-0 && docker-compose up -d --build
-cd ../gpu-1 && docker-compose up -d --build
-cd ../gpu-2 && docker-compose up -d --build
-cd ../gpu-3 && docker-compose up -d --build
-cd ../gpu-4 && docker-compose up -d --build
-cd ../gpu-5 && docker-compose up -d --build
-cd ../gpu-6 && docker-compose up -d --build
-cd ../gpu-7 && docker-compose up -d --build
