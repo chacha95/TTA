0418d5002bae467b7fa0053feec41637ee1ee104
chacha <ckwlsgur20@naver.com>
2021-06-21 09:25:36 +0900

    d

--- src/tta.py
+++ src/tta.py
@@ -167,31 +167,6 @@ class GeneralizedRCNNWithTTA(nn.Module):
         self.tta_mapper = DatasetMapperTTA(cfg)
         self.batch_size = batch_size
 
-    @contextmanager
-    def _turn_off_roi_heads(self, attrs):
-        """
-        Open a context where some heads in `model.roi_heads` are temporarily turned off.
-        Args:
-            attr (list[str]): the attribute in `model.roi_heads`
-        """
-        roi_heads = self.model.roi_heads
-        old = {}
-        for attr in attrs:
-            try:
-                old[attr] = getattr(roi_heads, attr)
-            except AttributeError:
-                # The head may not be implemented in certain ROIHeads
-                pass
-
-        if len(old.keys()) == 0:
-            yield
-        else:
-            for attr in old.keys():
-                setattr(roi_heads, attr, False)
-            yield
-            for attr in old.keys():
-                setattr(roi_heads, attr, old[attr])
-
     def _batch_inference(self, batched_inputs, detected_instances=None):
         """
         Execute inference on a list of inputs,
