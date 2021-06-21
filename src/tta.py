5a83fabc7581dd167942800406f0a66b343f522a
chacha <ckwlsgur20@naver.com>
2021-06-21 09:24:40 +0900

    d

--- src/tta.py
+++ src/tta.py
@@ -294,19 +294,3 @@ class GeneralizedRCNNWithTTA(nn.Module):
             self.cfg.TEST.DETECTIONS_PER_IMAGE,
         )
         return merged_instances
-
-    def _rescale_detected_boxes(self, augmented_inputs, merged_instances, tfms):
-        augmented_instances = []
-        for input, tfm in zip(augmented_inputs, tfms):
-            # Transform the target box to the augmented image's coordinate space
-            pred_boxes = merged_instances.pred_boxes.tensor.cpu().numpy()
-            pred_boxes = torch.from_numpy(tfm.apply_box(pred_boxes))
-
-            aug_instances = Instances(
-                image_size=input["image"].shape[1:3],
-                pred_boxes=Boxes(pred_boxes),
-                pred_classes=merged_instances.pred_classes,
-                scores=merged_instances.scores,
-            )
-            augmented_instances.append(aug_instances)
-        return augmented_instances
